8071bf42c2d6c4a22c51c042b88318a350f758b6
Ramin105 <132153746+Ramin105@users.noreply.github.com>
2025-04-17 09:20:39 +0400

    Update Basket.jsx

--- router/src/pages/Basket.jsx
+++ router/src/pages/Basket.jsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router";
 
 const Basket = () => {
-    const basket = JSON.parse(localStorage.getItem('basket')) || [];
-    const navigate = useNavigate();
+  const basket = JSON.parse(localStorage.getItem("basket")) || [];
+  const navigate = useNavigate();
 
-   
-
-    return (
-        <div>
-            <button onClick={() => navigate('/')}>Back to Products</button>
-            {basket.map(item => (
-  <div key={item.id} style={{ border: '1px solid #ccc', padding: '10px', margin: '10px' }}>
-    <img src={item.image} alt={item.title} style={{ width: '100px' }} />
-    <h3>{item.title}</h3>
-    <p>Price: ${item.price}</p>
-    <p>Quantity: {item.count}</p>
-    <p>Total: ${(item.price * item.count).toFixed(2)}</p>
-  </div>
-))}
-        </div>
-    );
+  return (
+    <div>
+      {" "} <button onClick={() => navigate("/")}>Back to Products</button>
+      <div style={{display:"flex"}}>
+       
+        {basket.map((item) => (
+          <div
+            key={item.id}
+            style={{
+              border: "1px solid #ccc",
+              padding: "10px",
+              margin: "10px",
+              width:'250px'
+            }}
+          >
+            <img src={item.image} alt={item.title} style={{ width: "100px" }} />
+            <h3>{item.title}</h3>
+            <p>Price: ${item.price}</p>
+            <p>Quantity: {item.count}</p>
+            <p>Total: ${(item.price * item.count).toFixed(2)}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Basket;
