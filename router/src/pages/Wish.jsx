b86986af5a74e108f153bad4d4582af3466581d5
Ramin105 <132153746+Ramin105@users.noreply.github.com>
2025-04-17 09:20:53 +0400

    Update Wish.jsx

--- router/src/pages/Wish.jsx
+++ router/src/pages/Wish.jsx
@@ -8,18 +8,18 @@ const Wish = () => {
    
 
     return (
-        <div>
-            <button onClick={() => navigate('/')}>Back to Products</button>
-            {wish.map(item => (
-  <div key={item.id} style={{ border: '1px solid #ccc', padding: '10px', margin: '10px' }}>
-    <img src={item.image} alt={item.title} style={{ width: '100px' }} />
-    <h3>{item.title}</h3>
-    <p>Price: ${item.price}</p>
-    <p>Quantity: {item.count}</p>
-    <p>Total: ${(item.price * item.count).toFixed(2)}</p>
-  </div>
+   <div> <button onClick={() => navigate('/')}>Back to Products</button>
+   <div style={{display:'flex', flexWrap:'wrap'}}>
+       
+       {wish.map(item => (
+<div  key={item.id} style={{ width:'250px', border: '1px solid #ccc', padding: '10px', margin: '10px' }}>
+<img src={item.image} alt={item.title} style={{ width: '100px' }} />
+<h3>{item.title}</h3>
+<p>Price: ${item.price}</p>
+
+</div>
 ))}
-        </div>
+   </div></div>
     );
 };
 
